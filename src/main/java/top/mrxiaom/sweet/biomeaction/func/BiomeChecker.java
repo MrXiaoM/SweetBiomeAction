409ce0feac3adbe96a883043ca23dc94b519d689
人間工作 <coolxiaom95@gmail.com>
2024-12-20 16:56:26 +0800

    load configuration

--- src/main/java/top/mrxiaom/sweet/biomeaction/func/BiomeChecker.java
+++ src/main/java/top/mrxiaom/sweet/biomeaction/func/BiomeChecker.java
@@ -3,20 +3,33 @@ package top.mrxiaom.sweet.biomeaction.func;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.NamespacedKey;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.MemoryConfiguration;
 import org.bukkit.entity.Player;
 import top.mrxiaom.pluginbase.func.AutoRegister;
+import top.mrxiaom.sweet.biomeaction.Actions;
 import top.mrxiaom.sweet.biomeaction.SweetBiomeAction;
 import top.mrxiaom.sweet.biomeaction.nms.NMS;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.UUID;
+import java.util.*;
 
 @AutoRegister
 public class BiomeChecker extends AbstractModule {
+    public static class Checker {
+        public final NamespacedKey biome;
+        public final long cooldownSecond;
+        public final List<String> commands;
+
+        Checker(NamespacedKey biome, long cooldownSecond, List<String> commands) {
+            this.biome = biome;
+            this.cooldownSecond = cooldownSecond;
+            this.commands = commands;
+        }
+    }
+    Set<String> blacklistWorlds = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
     Map<String, Long> cooldown = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
     Map<UUID, NamespacedKey> lastBiome = new HashMap<>();
+    Map<NamespacedKey, Checker> checkerMap = new TreeMap<>();
     public BiomeChecker(SweetBiomeAction plugin) {
         super(plugin);
         Bukkit.getScheduler().runTaskTimer(plugin, this::everySecond, 20L, 20L);
@@ -28,6 +41,31 @@ public class BiomeChecker extends AbstractModule {
         }
     }
 
+    @Override
+    public void reloadConfig(MemoryConfiguration config) {
+        super.reloadConfig(config);
+        Actions.titleFadeIn = config.getInt("title-time.fade-in", 10);
+        Actions.titleStay = config.getInt("title-time.stay", 30);
+        Actions.titleFadeOut = config.getInt("title-time.fade-out", 10);
+        blacklistWorlds.clear();
+        blacklistWorlds.addAll(config.getStringList("blacklist-worlds"));
+        checkerMap.clear();
+        ConfigurationSection section = config.getConfigurationSection("biomes");
+        if (section != null) for (String namespace : section.getKeys(false)) {
+            ConfigurationSection section1 = section.getConfigurationSection(namespace);
+            if (section1 != null) for (String value : section1.getKeys(false)) {
+                NamespacedKey biome = new NamespacedKey(namespace, value);
+                ConfigurationSection section2 = section1.getConfigurationSection(value + ".requirements");
+                if (section2 != null) for (String key : section2.getKeys(false)) {
+                    // TODO: 有空再写，命令触发条件 requirements
+                }
+                long cooldownSecond = section1.getLong(value + ".cooldown");
+                List<String> commands = section1.getStringList(value + ".commands");
+                checkerMap.put(biome, new Checker(biome, cooldownSecond, commands));
+            }
+        }
+    }
+
     public void check(Player player) {
         Location loc = player.getLocation();
         UUID uuid = player.getUniqueId();
@@ -35,7 +73,16 @@ public class BiomeChecker extends AbstractModule {
         NamespacedKey toBiome = NMS.getRealBiomeType(player.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
         if (toBiome != null && !toBiome.equals(fromBiome)) {
             lastBiome.put(uuid, toBiome);
-            // TODO: 触发操作 以及冷却时间
+            Checker checker = checkerMap.get(toBiome);
+            if (checker == null) return;
+            long now = System.currentTimeMillis();
+            String key = uuid + ";" + toBiome;
+            if (now < cooldown.getOrDefault(key, now)) {
+                // 冷却中
+                return;
+            }
+            cooldown.put(key, now + checker.cooldownSecond * 1000L);
+            Actions.execute(player, checker.commands);
         }
     }
 }
